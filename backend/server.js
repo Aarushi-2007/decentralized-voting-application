b3ed019795aadae36d55254e1584169cb1025f19
Aarushi <aarushigug.284@gmail.com>
2026-07-26 18:29:03 +0530

    fixed cors again :(

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const db= require('./db');
 app.use(express.json());
 app.use(
     cors({
-        origin:"https://decentralized-voting-application-ft.vercel.app",
+        origin:"decentralized-voting-application-lovat.vercel.app",
         credentials: true,
     })
 );
